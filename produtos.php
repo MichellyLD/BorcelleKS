827ac48de226e9eb81ce9dafb2a265dbf63ab5c5
michelly <michellylimad1@gmail.com>
2023-05-26 18:20:59 -0300

    produtos

---
+++ produtos.php
@@ -0,0 +1,150 @@
+<?php
+
+$produtos = [   
+    [        
+        "nome" => "Casinha tamanho real",
+        "foto" => "https://i.pinimg.com/564x/44/af/ea/44afeadb84b52a0f0d778ab8a0227439.jpg",
+        "preco" => "R$500",
+        "disponivel" => "Alugar por hora",
+        "descricao" => "Casinha de brinquedo tamanho real de dois andares com balanço e escorregador.",
+        "definido" => "alugar"
+    ],
+    [        
+        "nome" => "Moto de passeio com bebes",
+        "foto" => "https://i.pinimg.com/564x/9a/1e/01/9a1e016b5062ea0c84ce2c8a821b4bcf.jpg",
+        "preco" => "R$200",
+        "disponivel" => "Alugar por hora",
+        "descricao" => "Moto de passeio com bebes confrotavél e seguro para andar por qualquer lugar.",
+        "definido" => "alugar"
+    ],
+    [        
+        "nome" => "Caminhão de Brinquedo",
+        "foto" => "https://yupiibrinquedos.com.br/imgcache/2060/1000x/uploads/2060/product/photo_6182e0c22e9fd.jpg.webp",
+        "preco" => "R$ 29,99",
+        "disponivel" => "Trocar",
+        "descricao" => "Caminhão de brinquedo infantil, disponível para troca de até 7 dias úteis.",
+        "definido" => "trocar"//["trocar", "comprar"]????????????
+    ],
+    [        
+        "nome" => "Boneca de pano",
+        "foto" => "https://a-static.mlcdn.com.br/800x560/boneca-de-pano-macia-bebe-menina-30-cm-bbr-toys/presenteslw/9806985307/9713f2752691935140687e07b78d9fc7.jpeg",
+        "preco" => "R$ 19,99",
+        "disponivel" => "Trocar",
+        "descricao" => "Boneca de pano chegou! Aventuras e dirversão garantida, a melhor companhinha para o seu bebê.
+        ",
+        "definido" => "trocar"
+    ],
+    [
+        'nome' => 'Chocalho',
+        'foto' => 'https://ae01.alicdn.com/kf/HTB1f8sRbjnuK1RkSmFPq6AuzFXah.jpg',
+        'preco' => "R$14.99",
+        "disponivel" => "Trocar",
+        'descricao' => 'Chocalho de madeira, chame seus amiguinhos e monte uma banda de sucesso! O brinquedo perfeito para um dia de diversão.',
+        "definido" => "trocar"
+    ],
+    [
+        'nome' => 'Talco',
+        'foto' => 'https://res.cloudinary.com/beleza-na-web/image/upload/w_1500,f_auto,fl_progressive,q_auto:eco,w_800/v1/imagens/product/28282/11f532ca-1dff-4bc0-8eb9-70d0f9c06273-granado-bebe-tradicional-talco-100g.png',
+        'preco' => "R$14.99",
+        "disponivel" => "Comprar",
+        'descricao' => 'Chegou, chegouuu!Corra mamãe venha adiqrir o seu talco granado.',
+        "definido" => "comprar"
+    ],
+    [        
+        "nome" => "Mamadeira azul",
+        "foto" => "https://i.pinimg.com/564x/72/70/c4/7270c4751326f0833312e0e9598d4958.jpg",
+        "preco" => "R$30 a vista",
+        "disponivel" => "Comprar",     
+        "descricao" => "Conjunto de mamadeira e chupeta! Azul cor do mar, a melho opção para um lanche saboroso.Bico achatado e macio.",
+        "definido" => "comprar"
+    ],
+    [        
+        "nome" => "Mamadeira rosa",
+        "foto" => "https://i.pinimg.com/564x/a7/c8/64/a7c864fc3015996454b151e73f3021ac.jpg",
+        "preco" => "R$30 a vista",
+        "disponivel" => "Comprar",
+        "descricao" => "Conjunto de mamadeira e chupeta! Rosinha realeza, a melho opção para um lanche saboroso.Bico achatado e macio.",
+        "definido" => "comprar"
+    ],
+    [        
+        "nome" => "Carrinho",
+        "foto" => "https://www.obaudobebe.com.br/img/produtos/webp/carrinho_lila_cp2-5.webp",
+        "preco" => "R$250 a vista",
+        "disponivel" => "Comprar",
+        "descricao" => "Perfeito e aconchegante para passeios ou uso no dia a dia, com o máximo de conforto e versatilidade. Completo: moisés, rodas robustas, capota com mosquiteiro integrado e muito mais.",
+        "definido" => "trocar"
+    ],
+    [
+        "nome" => "Jumperoo",
+        "foto" => "https://www.obaudobebe.com.br/img/produtos/webp/jumperoo-descoberta-do-oceano-5.webp",
+        "preco" => "R$230",
+        "disponivel" => "Trocar",
+        "descricao" => "As crianças e os papais irão adorar essa Cadeira de Atividades Do fundo do mar da Winfun. Uma cadeirinha cheia de atividades para o bebê brincar e se alimentar!",
+        "definido" => "trocar"
+    ],
+    [        
+        "nome" => "Kit para bebes recém-nascidos azul",
+        "foto" => "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcSWjVr0wCnuLWsuYymif-9ybpYYdAU-f-_cSJJdzQ3RkwPQVYUpUtKLffFNkT4OzLI6Bjqy4pPlJZ6UIZf9W25R0RptxBkVh99tD8XIdZ0&usqp=CAE",
+        "preco" => "R$55",
+        "disponivel" => "Comprar",
+        "descricao" => "Adiquira já o kit cuidados blue e crie momentos especiais de spar com sua criança.",
+        "definido" => "comprar"
+    ],
+    [        
+        "nome" => "Kit para bebes recém-nascidos rosa",
+        "foto" => "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQ_39eGHAsMDmHf3Fp8o1G6T0oYnYOztg_taSRScrkUPSFVh7pbkdGv6kAHu41Y2kt2SP5e6uBUOU-Rn0E2cSaLH3wGHtKaUAkyBvD9di65ZSarVPBtsfUNaw&usqp=CAE",
+        "preco" => "R$55",
+        "disponivel" => "Comprar",
+        "descricao" => "Adiquira já o kit cuidados pink e crie momentos especiais de spar com sua criança.",
+        "definido" => "comprar"
+    ],
+    [        
+        "nome" => "Kit Bebê da Mustela",
+        "foto" => "https://www.bebedamour.com.br/images/produtos/1-mustela-bebe-kit-c-06-produtos031112.png",
+        "preco" => "R$129.99",
+        "disponivel" => "Comprar",
+        "descricao" => "Kit de produtos para cuidar da pele do seu bebê, cuidados e hidratação além de um cheirinho suave e leve.",
+        "definido" => "comprar"
+    ],
+    [        
+        "nome" => "Protex Baby Sabonete Líquido 400ml",
+        "foto" => "https://m.media-amazon.com/images/I/51u4p8cNaML._AC_SX425_.jpg",
+        "preco" => "R$15.30",
+        "disponivel" => "Comprar",
+        "descricao" => "Sabonete líquido infantil para bebê Protex Baby Proteção Delicada oferece proteção suave e delicada para o seu bebê.",
+        "definido" => "comprar"
+    ],
+    [        
+        "nome" => "Fraldas Pampers Confort Sec",
+        "foto" => "https://a-static.mlcdn.com.br/800x560/kit-fraldas-pampers-confort-sec-tam-xg-11-a-15kg-2-pacotes-com-62-unidades-cada/magazineluiza/229324300/9e0751da35e86fa7ec4f5f3e3cdfce1c.jpg",
+        "preco" => "R$80.30",
+        "disponivel" => "Comprar",
+        "descricao" => "Fraldas Pampers Confort Sec conforto e macio para seu bebê.",
+        "definido" => "comprar"
+    ],
+    [        
+        "nome" => "Tapete Gymini",
+        "foto" => "https://www.clubkidsoficial.com.br/produtos/tapete-gymini-safari-364.jpg?1599845998",
+        "preco" => "R$160.99",
+        "disponivel" => "Comprar",
+        "descricao" => "Tapete Gymini, cofortável e divertido para sue bebê ficar por horas e poder tirar um pequeno cochilo enquanto brinca.",
+        "definido" => "comprar"
+    ],
+    [        
+        "nome" => "Espaço de brinquedos para bebes",
+        "foto" => "https://brinquedosparafesta.com/wp-content/uploads/2018/04/espa%C3%A7o-kids-baby1.jpeg",
+        "preco" => "R$169.99",
+        "disponivel" => "Alugar por hora",
+        "descricao" => "Espaço e brinquedo par crianças e bebês, grande espaço com capacidade de ate 12 crianças, com grade de porteção e diversos brinquedos feitos com o maior cuidado possível.",
+        "definido" => "alugar"
+    ],
+    [        
+        "nome" => "Piscina de Bolinhas",
+        "foto" => "https://images.tcdn.com.br/img/img_prod/808318/piscina_de_bolinhas_baby_disney_play_park_com_motor_soprador_385_1_20200824151917.jpg",
+        "preco" => "R$160",
+        "disponivel" => "Alugar por hora",
+        "descricao" => "Piscina de bolinhas inflável com tematica da disney, seguro e divertido para todas as crianças.",
+        "definido" => "alugar"
+    ]
+
+];
